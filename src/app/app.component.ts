2bfb3fef8628b82c4f06bff7f5c45ad6b8fa40bb
Shazibali Gesawat <shazibali@icloud.com>
2018-06-14 05:21:13 +0530

    HEROKU DEPLOYMENT.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = Login;
 
   @ViewChild(Nav) nav: Nav;
   
