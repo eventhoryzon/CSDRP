f4a8ffd3374d59c02c0848d8d6bfbc4165370374
Shazibali Gesawat <shazibali@icloud.com>
2018-06-15 07:49:52 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     twitterAuth : {
-        consumerKey       : '3sayP0KbP2JaeaoYTI4Q57VUe',
-        consumerSecret    : '6WqIhrNFNSEooJE8qMYmDthMZxQIQxsDanhubYoyHRIHLu1pQ4',
+        consumerKey       : '',
+        consumerSecret    : '',
         callbackURL      : 'http://localhost:8080/auth/twitter/callback',
         userProfileURL    : 'https://api.twitter.com/1.1/account/verify/verify_credentials.json?include_email=true'
     },
